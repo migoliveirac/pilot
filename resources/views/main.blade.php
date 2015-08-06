aaf4343f84f3ef0f1791ff063ad5d2e1fa1052ca
Miguel Couto <mig.oliveira.c@gmail.com>
2015-08-06 19:58:04 +0100

    Add homepage

---
+++ resources/views/main.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    @yield('header')
+
+</head>
+
+<body>
+
+    @yield('body');
+
+    <!-- Footer -->
+    <footer>
+        @yield('footer')
+    </footer>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
